cfd7dbe03ccffdcb904ec9c2f4a0da3735105bae
Ludovic Jozeau <federandink@gmail.com>
2021-05-09 22:04:17 +0200

    remove document symbols from macro expansion
    eg:
    
     #define M                                                                               \
      namespace ns_name                                                                      \
      {                                                                                      \
      namespace ns_name_impl                                                                 \
      {                                                                                      \
      void f() {}                                                                            \
      }                                                                                      \
      }
    
    M;

--- src/messages/textDocument_document.cc
+++ src/messages/textDocument_document.cc
@@ -119,6 +119,20 @@ template <> bool ignore(const QueryType::Def *def) { return !def || def->kind ==
 template <> bool ignore(const QueryVar::Def *def) { return !def || def->is_local(); }
 } // namespace
 
+bool is_macro_expansion(DB *db, WorkingFile *wf, QueryFile *file,
+                        DocumentSymbol const &ds) noexcept {
+  if (ds.kind == SymbolKind::Macro) {
+    return false;
+  }
+  auto start = ds.range.start;
+  auto syms = findSymbolsAtLocation(wf, file, start, true);
+
+  auto end = std::end(syms);
+  return std::find_if(std::begin(syms), end, [db](SymbolRef sym) {
+           return getSymbolKind(db, sym) == SymbolKind::Macro;
+         }) != end;
+}
+
 void MessageHandler::textDocument_documentSymbol(JsonReader &reader, ReplyOnce &reply) {
   DocumentSymbolParam param;
   reflect(reader, param);
@@ -166,7 +180,7 @@ void MessageHandler::textDocument_documentSymbol(JsonReader &reader, ReplyOnce &
           if (auto range1 = getLsRange(wf, sym.extent); range1 && range1->includes(*range))
             ds->range = *range1;
       }
-      withEntity(db, sym, [&](const auto &entity) {
+      withEntity(db, sym, [&, wf = wf, file = file](const auto &entity) {
         const auto *def = entity.anyDef();
         if (!def)
           return;
@@ -174,7 +188,9 @@ void MessageHandler::textDocument_documentSymbol(JsonReader &reader, ReplyOnce &
         ds->detail = def->detailed_name;
         ds->kind = def->kind;
 
-        if (!ignore(def) && (ds->kind == SymbolKind::Namespace || allows(sym))) {
+        if (!ignore(def) &&
+            (ds->kind == SymbolKind::Namespace || allows(sym)) &&
+            !is_macro_expansion(db, wf, file, *ds)) {
           // Drop scopes which are before selectionRange.start. In
           // `int i, j, k;`, the scope of i will be ended by j.
           while (!scopes.empty() && scopes.back()->range.end <= ds->selectionRange.start)
